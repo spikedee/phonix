a72c1927c6cbee7b628f8fbc7bde3095396f9906
jaspax <jaspax@c8c96672-cb5c-11de-b058-13285e7dd63e>
2010-02-03 04:11:27 +0000

    added diacritic symbol sets and tests
    git-svn-id: https://phonix.googlecode.com/svn/trunk@47 c8c96672-cb5c-11de-b058-13285e7dd63e

--- UnitTest/Resx.cs
+++ UnitTest/Resx.cs
@@ -57,9 +57,21 @@ namespace Phonix.UnitTest
         }
 
         [Test]
+        public void ParseStdSymbolsDiacritics()
+        {
+            ParseResource("std.features", "std.symbols.diacritics");
+        }
+
+        [Test]
         public void ParseStdSymbolsIpa()
         {
             ParseResource("std.features", "std.symbols.ipa");
+        }
+
+        [Test]
+        public void ParseStdSymbolsIpaDiacritics()
+        {
+            ParseResource("std.features", "std.symbols.ipa.diacritics");
         }
 
         [Test]
@@ -79,6 +91,8 @@ namespace Phonix.UnitTest
             Phonix.Parse.Util.ParseFile(ascii, "ascii", "std.symbols");
             Phonix.Parse.Util.ParseFile(ipa, "ipa", "std.symbols.ipa");
 
+            Assert.IsTrue(ascii.SymbolSet.BaseSymbols.Count > 0);
+            Assert.IsTrue(ascii.SymbolSet.Diacritics.Count == 0);
             Assert.AreEqual(ascii.SymbolSet.Count, ipa.SymbolSet.Count);
             foreach (Symbol s in ascii.SymbolSet)
             {
@@ -91,6 +105,33 @@ namespace Phonix.UnitTest
                 {
                     Assert.Fail("No match in ipa for {0} {1}", s, s.FeatureMatrix);
                 }
+            }
+        }
+
+        [Test]
+        public void DiacriticSymbolEquivalence()
+        {
+            // this test exists to ensure that the diacritics in the ascii set
+            // and the ipa set are the same
+
+            // set up two phonologies with the same features
+            var ascii = ParseResource("std.features");
+            var ipa = new Phonology();
+            foreach (var f in ascii.FeatureSet)
+            {
+                ipa.FeatureSet.Add(f);
+            }
+
+            Phonix.Parse.Util.ParseFile(ascii, "ascii", "std.symbols.diacritics");
+            Phonix.Parse.Util.ParseFile(ipa, "ipa", "std.symbols.ipa.diacritics");
+
+            Assert.IsTrue(ascii.SymbolSet.BaseSymbols.Count == 0);
+            Assert.IsTrue(ascii.SymbolSet.Diacritics.Count > 0);
+            Assert.AreEqual(ascii.SymbolSet.Count, ipa.SymbolSet.Count);
+            foreach (Diacritic d in ascii.SymbolSet.Diacritics.Values)
+            {
+                Assert.AreEqual(1, 
+                        ipa.SymbolSet.Diacritics.Values.Where(s => s.FeatureMatrix.Equals(d.FeatureMatrix)).Count());
             }
         }
 
